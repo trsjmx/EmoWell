4b1f4ad55540d7f14fcc8ea8714ca7abf98aa7bc
trsjmx <tawfiq1113@gmail.com>
2024-10-01 14:30:31 +0300

    Page2

---
+++ Emotional_Wellness/IntroPage2.swift
@@ -0,0 +1,18 @@
+//
+//  IntroPage2.swift
+//  Emotional_Wellness
+//
+//  Created by Eng.Arwa on 28/03/1446 AH.
+//
+
+import SwiftUI
+
+struct IntroPage2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroPage2()
+}
